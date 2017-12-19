afd487c21326e46a362c31dc7a72aa0ac61045ac
youzhen.yz <youzhen.yz@alibaba-inc.com>
2017-12-19 20:47:34 +0800

    修改线程创建程序

--- ThreadCreate/ThreadCreate/main.cpp
+++ ThreadCreate/ThreadCreate/main.cpp
@@ -2,6 +2,9 @@
 #include <process.h>
 #include <windows.h>
 
+const int threadNum = 10;
+int g_count = 0;
+CRITICAL_SECTION g_section;
 /**
  * @Brief: CreateThread子线程运行函数
  */
@@ -23,36 +26,38 @@ unsigned int _stdcall BeginThreadRun(PVOID pM){
  */
 unsigned int _stdcall CountThreadRun(void* arg){
 	int number = *(int*)arg;
-	int count = *((char*)arg + sizeof(int));
-	printf("线程%d启动，计数:%d\n", number, count);
+    EnterCriticalSection(&g_section);
+    for (int i = 0; i < 10000; i++)
+    {
+        g_count++;
+    }
+    LeaveCriticalSection(&g_section);
+    printf("线程%d启动，计数:%d\n", number, g_count);
+    Sleep(0);
 	return 0;
 }
 
 int main(){
-	static int g_count = 0;
-	char arg[10] = {0};
+    InitializeCriticalSection(&g_section);
 	printf("主线程启动\n");
 	LPDWORD threadId = 0;
 	HANDLE createHandle = CreateThread(NULL, 0, CreateThreadRun, NULL, 0, threadId);
 	HANDLE beginHandle = (HANDLE)_beginthreadex(NULL,0, BeginThreadRun, NULL, 0, NULL);
 	WaitForSingleObject(createHandle, INFINITE);
 	WaitForSingleObject(beginHandle, INFINITE);
-	HANDLE countHandle[10];
-	for (int i = 0; i < 10; i++)
+    CloseHandle(createHandle);
+    CloseHandle(beginHandle);
+	HANDLE countHandle[threadNum];
+	for (int i = 0; i < threadNum; i++)
 	{
-		g_count++;
-		memset(arg, 0, sizeof(char) * 10);
-		memcpy(arg, &i, sizeof(int));
-		memcpy(arg + sizeof(int), &(g_count), sizeof(int));
-		int number = *(int*)arg;
-		int count = *((char*)arg + sizeof(int));
-		countHandle[i] = (HANDLE)_beginthreadex(NULL, 0, CountThreadRun, arg, 0, NULL);
+		countHandle[i] = (HANDLE)_beginthreadex(NULL, 0, CountThreadRun, &i, 0, NULL);
 	}
-	WaitForMultipleObjects(10, countHandle, TRUE, INFINITE);
+	WaitForMultipleObjects(threadNum, countHandle, TRUE, INFINITE);
 	for(int i = 0; i < threadNum; i++)
 	{
 		CloseHandle(countHandle[i]);
 	}
+    DeleteCriticalSection(&g_section);
 	getchar();
 	return 0;
 }
