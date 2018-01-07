dc150c9cb33e61fec20313c4bbf9d79cf442b0c0
youzhen.yz <youzhen.yz@alibaba-inc.com>
2018-01-07 11:14:41 +0800

    关键区域增加同步条件，使之可以实现线程同步

--- CriticalSectionThread/CriticalSectionThread/main.cpp
+++ CriticalSectionThread/CriticalSectionThread/main.cpp
@@ -8,15 +8,17 @@ const int threadNum = 10;
 /* 关键字段区域 */
 CRITICAL_SECTION g_MainThreadSection;
 CRITICAL_SECTION g_SubThreadSection;
+CONDITION_VARIABLE g_MainThreadCondition;
 
 unsigned int _stdcall ThreadRun(void* pM){
-    int* number = (int*)pM;
-    LeaveCriticalSection(&g_MainThreadSection);
+    int number = *(int*)pM;
+    WakeConditionVariable(&g_MainThreadCondition);
+    //LeaveCriticalSection(&g_MainThreadSection);
     Sleep(0);
     EnterCriticalSection(&g_SubThreadSection);
     InterlockedIncrement(&g_count);
     Sleep(0);
-    printf("The Thread Num is : %d and the global is %d\n", *number, g_count);
+    printf("The Thread Num is : %d and the global is %d\n", number, g_count);
     LeaveCriticalSection(&g_SubThreadSection);
     return 0;
 }
@@ -25,19 +27,20 @@ int main(){
     /* 初始化关键字段 */
     InitializeCriticalSection(&g_MainThreadSection);
     InitializeCriticalSection(&g_SubThreadSection);
-
+    InitializeConditionVariable(&g_MainThreadCondition);
     HANDLE *threadHandle = new HANDLE[threadNum];
     for (int i = 0; i < threadNum; i++)
     {
-        EnterCriticalSection(&g_MainThreadSection);
         threadHandle[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadRun,&i, true, NULL);
+        //EnterCriticalSection(&g_MainThreadSection);
+        SleepConditionVariableCS(&g_MainThreadCondition, &g_MainThreadSection, INFINITE);
     }
     WaitForMultipleObjects(threadNum, threadHandle, true, INFINITE);
     for(int i = 0; i < threadNum; i++)
-	{
-		CloseHandle(threadHandle[i]);
-	}
-	delete[] threadHandle;
+    {
+        CloseHandle(threadHandle[i]);
+    }
+    delete[] threadHandle;
     threadHandle = NULL;
     DeleteCriticalSection(&g_MainThreadSection);
     DeleteCriticalSection(&g_SubThreadSection);
